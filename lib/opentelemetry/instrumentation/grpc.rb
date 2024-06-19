b9a9b095a051b7955a4c9f27b0a52c6bb9ddfad2
Hazel Bachrach <hbachrach@dropbox.com>
2024-06-18 21:37:37 -0700

    chore: initial commit

---
+++ lib/opentelemetry/instrumentation/grpc.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "grpc/version"
+
+module Opentelemetry
+  module Instrumentation
+    module Grpc
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/opentelemetry/instrumentation/grpc/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Opentelemetry
+  module Instrumentation
+    module Grpc
+      VERSION = "0.1.0"
+    end
+  end
+end
